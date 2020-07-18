b05b2ca384838c789fb39c6d2401611beccedc5f
Gufaran Mujawar <gufaranmujawar@gmail.com>
2020-07-18 10:38:39 +0530

    Error of web is resolved
    path error

--- Client/restaurantmanagement/src/app/app.module.ts
+++ Client/restaurantmanagement/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserLoginComponent } from './user-login/user-login/user-login.component';
-import { OrderDashboardComponent } from './order-dashboard/order-dashboard/order-dashboard.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ViewProductsComponent } from './view-products/view-products.component';
