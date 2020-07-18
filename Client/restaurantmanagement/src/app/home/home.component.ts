b146e7ec8c62a790b3252e85d56950a6eac2f8ba
Gufaran Mujawar <gufaranmujawar@gmail.com>
2020-07-18 16:32:43 +0530

    Home page added
    Demo home page added in code

--- Client/restaurantmanagement/src/app/home/home.component.ts
+++ Client/restaurantmanagement/src/app/home/home.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  food_items: any[] = [" Chicken Breast ", "  Fresh Grilled Asparagus", "Pork-Stuffed Skins", " Grilled Chicken", " Steak Frites"];
+  public menu: {}[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    let menus = document.getElementsByClassName('.list');
+    for (let i = 0; i < menus.length; i++) {
+      menus[i].innerHTML = "<li>" + this.food_items[0] + "<span>" + "................." + "</span>" + "<b>" + "$19.50" + "</b>" + "</li>"
+        + "<li>" + this.food_items[1] + "<span>" + "......" + "</span>" + "<b>" + "$25.00" + "</b>" + "</li>"
+        + "<li>" + this.food_items[2] + "<span>" + "............." + "</span>" + "<b>" + "$25.50" + "</b>" + "</li>"
+        + "<li>" + this.food_items[3] + "<span>" + "................." + "</span>" + "<b>" + "$25.49" + "</b>" + "</li>"
+        + "<li>" + this.food_items[4] + "<span>" + "....................." + "</span>" + "<b>" + "$25.00" + "</b>" + "</li>";
+    }
+
   }
+
+  
 
 }
